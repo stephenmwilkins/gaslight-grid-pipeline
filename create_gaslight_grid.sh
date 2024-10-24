da53b7a2b47dc2643ed1cb8b7f7e62e9716bd8ad
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-10-24 09:31:22 +0100

    first pipeline commit

---
+++ create_gaslight_grid.sh
@@ -0,0 +1,31 @@
+incident_grid_dir='/research/astrodata/highz/synthesizer/grids'
+grid_dir='/research/astrodata/highz/gaslight/grids'
+output_dir='/research/astrodata/highz/gaslight/cloudy'
+
+# standard SPS grid
+# incident_grids=('bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.')
+# config_files=('c23.01-full')
+
+# standard SPS grid
+incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.")
+config_file=("sps-c23.01-test")
+
+# standard AGN grid
+incident_grids=( "qsosed-isotropic-limited" )
+config_files=( "agn-c23.01-reduced-v1" )
+
+cd ..
+
+for i in "${!incident_grids[@]}"; do
+
+    incident_grid=${incident_grids[$i]}
+    config_file=${config_files[$i]}
+    # run the setup script
+    python create_gaslight_grid.py -grid_dir=$grid_dir -incident_grid_dir=$incident_grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir
+
+done
+
+
+
+
+
---
+++ setup_grid_creation.sh
@@ -0,0 +1,47 @@
+
+# runs the setup script and optionally runs a single grid point
+
+# apollo2
+grid_dir='/research/astrodata/highz/synthesizer/grids'
+output_dir='/its/home/sw376/astrodata/gaslight/cloudy'
+cloudy_dir='/research/astro/flare/software/cloudy/'
+machine='apollo2'
+
+# local
+# grid_dir='/Users/sw376/Dropbox/Research/data/synthesizer/grids'
+# output_dir='/Users/sw376/Dropbox/Research/data/gaslight/cloudy'
+# cloudy_dir='/Users/sw376/Dropbox/Research/software/cloudy'
+
+# standard SPS grid
+# incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.")
+# config_file=("c23.01-full")
+
+# standard SPS grid
+incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.")
+config_file=("sps-c23.01-test")
+
+# standard AGN grid
+incident_grids=( "qsosed-isotropic-limited" )
+config_files=( "agn-c23.01-v1" )
+
+cd ..
+
+for i in "${!incident_grids[@]}"; do
+
+    incident_grid=${incident_grids[$i]}
+    config_file=${config_files[$i]}
+    # run the setup script
+    python setup_grid_creation.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -machine=$machine
+
+    # now run a single grid point
+    # index=1
+    # python run_cloudy.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -index=$index
+
+done
+
+
+
+
+
+
+
