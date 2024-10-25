7e37bf0ad0650e18eba5a65a5c04ea101654ed4a
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-10-25 13:44:06 +0100

    udpated pipeline

--- setup_grid_creation.sh
+++ setup_grid_creation.sh
@@ -17,12 +17,17 @@ machine='apollo2'
 # config_files=("c23.01-full")
 
 # standard SPS grid
-incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6,7,8")
-config_files=("sps-c23.01-test-v1")
+incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0")
+config_files=("sps-c23.01-v1")
 
 # standard AGN grid
 # incident_grids=( "qsosed-isotropic-limited" )
 # config_files=( "agn-c23.01-v1" )
+
+incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0"  "qsosed-isotropic-limited")
+config_files=("sps-c23.01-v1" "agn-c23.01-v1")
+
+
 
 cd ..
 
