a4891d9903f84418925e01c5e8b889447d43051a
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-11-19 09:35:44 +0000

    updated pipeline

--- create_gaslight_grid.sh
+++ create_gaslight_grid.sh
@@ -26,11 +26,14 @@ config_files=("agn-c23.01-extremes-v1")
 incident_grids=("qsosed-isotropic-limited")
 config_files=("agn-c23.01-reduced-v1")
 
-incident_grids=("bpl-feltre16" "bpl-feltre16")
-config_files=("agn-c23.01-extremes-v1" "agn-c23.01-reduced-v1")
+incident_grids=("bpl-feltre16")
+config_files=("agn-c23.01-reduced-v1")
 
 incident_grids=("qsosed-isotropic-limited")
 config_files=("agn-c23.01-v1")
+
+incident_grids=("bpl-feltre16" )
+config_files=("agn-c23.01-extremes-v1")
 
 cd ..
 
