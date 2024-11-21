71b99d2cd6a934ff816bc053fb2189dee919d7aa
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-11-21 13:13:40 +0000

    updated pipeline

--- create_gaslight_grid.sh
+++ create_gaslight_grid.sh
@@ -41,6 +41,17 @@ config_files=("agn-c23.01-reduced-v1" "agn-c23.01-v1")
 incident_grids=("qsosed-isotropic-limited")
 config_files=("agn-c23.01-extremes-v1")
 
+incident_grids=("qsosed-isotropic-limited" "qsosed-isotropic-limited" "qsosed-isotropic-limited" "qsosed-isotropic-limited")
+config_files=("agn-c23.01-test-v1" "agn-c23.01-extremes-v1" "agn-c23.01-reduced-v1" "agn-c23.01-v1")
+
+incident_grids=("bpl-feltre16" "bpl-feltre16" "bpl-feltre16" "bpl-feltre16")
+config_files=("agn-c23.01-test-v1" "agn-c23.01-extremes-v1" "agn-c23.01-reduced-v1" "agn-c23.01-v1")
+
+incident_grids=("qsosed-isotropic-limited" "qsosed-isotropic-limited" "qsosed-isotropic-limited" "qsosed-isotropic-limited" "bpl-feltre16" "bpl-feltre16" "bpl-feltre16" "bpl-feltre16")
+config_files=("agn-c23.01-test-v1" "agn-c23.01-extremes-v1" "agn-c23.01-reduced-v1" "agn-c23.01-v1" "agn-c23.01-test-v1" "agn-c23.01-extremes-v1" "agn-c23.01-reduced-v1" "agn-c23.01-v1")
+
+
+
 cd ..
 
 for i in "${!incident_grids[@]}"; do
